eb4a1e027fac8d8f39f1e329b9fbc103b2cf5e33
Alan Chan <alanchendecai@hotmail.com>
2020-11-10 15:29:38 +0800

    [add] udemy docker [20201110] Inserted sample 158

---
+++ api_app/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  red_Host: process.env.red_host,
+  red_Port: process.env.red_port,
+  pg_Uid: process.env.pg_uid,
+  pgHost: process.env.pg_host,
+  pg_DB: process.env.pg_db,
+  pg_Pwd: process.env.pg_pwd,
+  pgPort: process.env.pg_port,
+};
---
+++ wkr_app/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  red_Host: process.env.red_host,
+  red_Port: process.env.red_port,
+};
